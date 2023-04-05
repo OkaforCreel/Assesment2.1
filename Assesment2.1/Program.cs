39af2457a524f3d076aca42432f504479ae53f9b
Creel Okafor <okaforcreel@gmail.com>
2023-04-05 17:02:35 +0100

    Add project files.

---
+++ Assesment2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assesment2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "alic3", "bob", "3", "4", "00000" };
+            int maxVal = 0;
+
+            foreach (string s in strs)
+            {
+                int val;
+                if (int.TryParse(s, out val))
+                {
+                    maxVal = Math.Max(maxVal, val);
+                }
+                else
+                {
+                    maxVal = Math.Max(maxVal, s.Length);
+                }
+            }
+            Console.WriteLine("Maximum value: " + maxVal);
+        }
+    }
+}
